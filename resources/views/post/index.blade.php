a62a3a337b7bff2f48335e44ece281f6499fd654
Hussein Hossaini <euh.sayed@gmail.com>
2026-01-06 22:15:01 +0200

    Create Form using Tailwind

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout title="Blog Posts">
-   
+
+    <div class="mb-6 flex items-center justify-end gap-x-6">
+        <a href="/blog/create"
+            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
+            New Post</a>
+    </div>
 
     @forelse ($posts as $post)
         <div class="mb-4 p-4 border rounded-lg shadow-sm">
